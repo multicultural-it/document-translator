62c075870e1dbd0dbd6e8671ce17feae3797869e
German Burgardt <german@getgloby.com>
2023-10-25 23:41:05 -0300

    translate improve agent (uniquie agent)

--- src/services/translate-improve-paragraph.js
+++ src/services/translate-improve-paragraph.js
@@ -4,12 +4,12 @@ const RETRY_LIMIT = 3;
 
 const SYSTEM_PROMPT = `You are a specialist in assembling coherent translations using node translations and ensuring cultural appropriateness. The target language is {targetLanguage}. When given an original paragraph and an array of nodes with their original text, and indices, your primary task is to construct a coherent translated paragraph. If the concatenated node translations don't make sense or don't match the paragraph's meaning, rearrange the nodes and adjust the translations if needed. Ensure to keep the nodes in a specific order, defined by their indices. Furthermore, make sure your translations are clear, engaging, and culturally appropriate. Remember to escape special characters like quotes and apostrophes. Keep in mind that your response will be processed using the JSON.parse() function.`;
 
-const USER_PROMPT_TEMPLATE = `Given a paragraph and its node original text and translations, your primary task is to ensure that the concatenated node translations provide a coherent and culturally appropriate translation of the entire paragraph. If the current order of nodes doesn't produce a clear translation, rearrange the nodes and adjust the translations accordingly. The translation should resonate with {targetLanguage} speakers. Ensure the resulting paragraph maintains the original context, meaning, and cultural nuances. Remember to escape special characters like quotes and apostrophes, and be aware that your response will be processed using the JSON.parse() function. The example below illustrates how you should proceed.
+const USER_PROMPT_TEMPLATE = `Given a paragraph and its node original text and translations, your primary task is to ensure that the concatenated node translations provide a coherent and culturally appropriate translation of the entire paragraph. If the current order of nodes doesn't produce a clear translation, rearrange the nodes and adjust the translations accordingly. The translation should resonate with {targetLanguage} speakers. Ensure the resulting paragraph maintains the original context, meaning, and cultural nuances. Remember to escape special characters like quotes and apostrophes, and be aware that your response will be processed using the JSON.parse() function. The example below illustrates how you should proceed. Pay attention to how some texts are changed from one node to another if the indices are changed. That is important if necessary.
 
 Example:
 Input:
 {
-  "paragraph": "This course can help you better understand LinkedIn Marketing Solutions’ paid products.",
+  "paragraph": "This course can help you better understand LinkedIn Marketing Solutions’ paid products. So, if you’re a marketer, salesperson, or agency, this course can help you.",
   "nodes": [
     {
       "index": 1,
@@ -21,13 +21,13 @@ Input:
     },
     {
       "index": 3,
-      "text": "’ paid products."
+      "text": "’ paid products. So, if you’re a marketer, salesperson, or agency, this course can help you."
     }
   ]
 }
 Output:
 {
-  "paragraph": "Este curso puede ayudarlo a \\"comprender\\" mejor los productos pagos de LinkedIn Marketing Solutions.",
+  "paragraph": "Este curso puede ayudarlo a \\"comprender\\" mejor los productos pagos de LinkedIn Marketing Solutions. Por lo tanto, si eres un comercializador, vendedor o agencia, este curso puede ayudarte.",
   "nodes": [
     {
       "index": 1,
@@ -39,7 +39,7 @@ Output:
     },
     {
       "index": 2,
-      "translation": "de LinkedIn Marketing Solutions."
+      "translation": "de LinkedIn Marketing Solutions. Por lo tanto, si eres un comercializador, vendedor o agencia, este curso puede ayudarte."
     }
   ]
 }
